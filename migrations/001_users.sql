361a0c3d4b7bcd0177621c3fc12bfabe964291b9
AdityaTaggar05 <aditya05.mgg@gmail.com>
2026-01-02 22:07:06 +0530

    Fixed some minor issues

--- migrations/001_users.sql
+++ migrations/001_users.sql
@@ -2,5 +2,7 @@ CREATE TABLE users (
 	id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
 	email TEXT UNIQUE NOT NULL,
 	password_hash TEXT NOT NULL,
+	role TEXT NOT NULL DEFAULT 'user',
+	email_verified BOOLEAN NOT NULL DEFAULT FALSE,
 	created_at TIMESTAMP DEFAULT now()
 );
