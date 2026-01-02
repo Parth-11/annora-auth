4d67e9df13079d6fcb118fa764139ff580c1662e
AdityaTaggar05 <aditya05.mgg@gmail.com>
2026-01-02 15:07:00 +0530

    Fixed password validation

--- internal/service/auth/validation.go
+++ internal/service/auth/validation.go
@@ -1,14 +1,34 @@
 package authservice
 
-import "regexp"
+import (
+	"regexp"
+	"unicode"
+)
 
 var emailRegex = regexp.MustCompile(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`)
-var passwordRegex = regexp.MustCompile(`^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\W)(?!.* ).{8,16}$`)
 
 func isValidEmail(email string) bool {
 	return emailRegex.MatchString(email)
 }
 
 func isValidPassword(password string) bool {
-	return passwordRegex.MatchString(password)
+	if len(password) < 8 || len(password) > 16 || regexp.MustCompile(`\s`).MatchString(password) {
+		return false
+	}
+
+	var hasNumber, hasUpperCase, hasLowerCase, hasSpecial bool
+	for _, c := range password {
+		switch {
+			case unicode.IsNumber(c):
+				hasNumber = true
+			case unicode.IsUpper(c):
+				hasUpperCase = true
+			case unicode.IsLower(c):
+				hasLowerCase = true
+			case unicode.IsPunct(c) || unicode.IsSymbol(c):
+				hasSpecial = true
+		}
+	}
+
+	return hasNumber && hasUpperCase && hasLowerCase && hasSpecial
 }
